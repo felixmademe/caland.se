0c3944157a673034d3fb162d568bba32ca5283b2
Felix Wetell <felix.wetell@gmail.com>
2019-02-04 17:38:34 +0100

    Added content to landing page #9

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,56 @@
 @section( 'title', 'Start' )
 @section( 'content' )
 
-    <h1>Caland AB</h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <div class="mobile-hero d-block d-md-none">
+        <img src="{{ asset( '/img/collection.jpg' ) }}" alt="">
+        <div class="text">
+            <h1>Caland AB</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <a class="btn btn-red" href="{{ route( 'about' ) }}">Läs mer</a>
+        </div>
+    </div>
+
+    <div class="hero d-none d-md-block">
+        <div class="d-flex flex-row">
+            <div class="col-md-6 text-half d-flex align-items-center">
+                <div class="">
+                    <h1>Caland AB</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <a class="btn btn-red" href="{{ route( 'about' ) }}">Läs mer</a>
+                </div>
+            </div>
+            <div class="col-md-6 img-half">
+                <img src="{{ asset( '/img/collection.jpg' ) }}" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="clients text-center d-flex align-items-center">
+            <div class="client-swiper swiper-container">
+                <h2>Några av våra stolta kunder</h2>
+                <br><br>
+                <div class="swiper-wrapper swiper-no-swiping">
+                    <div class="swiper-slide align-self-center">
+                        <img src="{{ asset( '/img/peab.svg' ) }}" alt="">
+                    </div>
+                    <div class="swiper-slide align-self-center">
+                        <img src="{{ asset( '/img/valmet.jpg' ) }}" alt="">
+                    </div>
+                    <div class="swiper-slide align-self-center">
+                        <img src="{{ asset( '/img/dhl.png' ) }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
