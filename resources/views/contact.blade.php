332497f53a1585fedebb446997ac820bcef64e8a
Felix Wetell <felix.wetell@gmail.com>
2019-02-04 11:00:52 +0100

    Added the basic code for new views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+@extends( 'layouts.app' )
+@section( 'title', 'Kontakt' )
+@section( 'content' )
+
+@endsection
---
+++ resources/views/services.blade.php
@@ -0,0 +1,5 @@
+@extends( 'layouts.app' )
+@section( 'title', 'Tjänster' )
+@section( 'content' )
+
+@endsection
