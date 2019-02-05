130c4b23278b10be231c1ad4edb06f33112ef663
Felix Wetell <felix.wetell@gmail.com>
2019-02-05 19:30:40 +0100

    Added content to contact view #18

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,34 @@
 @section( 'title', 'Kontakt' )
 @section( 'content' )
 
+    <div class="contact p-5">
+        <div class="text-center">
+            <h1>Kontakta oss</h1>
+            <p>
+                Hittar du svar på dina frågor?
+                Du är självklart välkommen att ringa oss eller så kan du skriva till oss.
+                Det kan du göra genom att använda formuläret nedan.
+            </p>
+        </div>
+        <form class="" action="" method="post">
+            <div class="form-group">
+                <label for="name">Namn</label>
+                <input class="form-control" type="text" name="name" placeholder="Namn">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" type="text" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="subject">Ämne</label>
+                <input class="form-control" type="text" name="subject" placeholder="Ämne">
+            </div>
+            <div class="form-group">
+                <label for="message">Meddelande</label>
+                <textarea class="form-control" type="text" name="message" placeholder="Meddelande" rows="5"></textarea>
+            </div>
+            <button type="submit" class="btn btn-red btn-expand">Skicka</button>
+        </form>
+    </div>
+
 @endsection
