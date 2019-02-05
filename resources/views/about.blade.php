2e333c9d563bae5d811c5efb06e1b4ba4c99ce99
Felix Wetell <felix.wetell@gmail.com>
2019-02-05 20:11:26 +0100

    Added a timeline on about page #17

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,64 @@
 @section( 'title', 'Om oss' )
 @section( 'content' )
 
+    <section class="about">
+        <div class="text-center">
+            <h1>Om oss</h1>
+        </div>
+        <p>
+            Caland är ett familjeägt företag som startades år 1994 genom det första bolaget Caland AB.
+            Företagsnamnet är de tre första bokstäverna från familjenamnen Calais och Andersson,
+            <span class="red">CAL</span> och <span class="red">AND</span> blev företagsnamnet CALAND.
+            <br><br>
+            Vid starten sysselsatte företaget två personer som servade företag inom åkeri- och betonghåltagningsbranschen genom personaluthyrning.
+            Nu har företaget växt från att bedrivas från familjens bostad, flytt till olika lokaler i Skattkärr till att slutligen 2014 ha
+            färdigställt en nybyggd industrifastighet på Herrövägen i Skattkärr. Idag  sysselsätter Calandkoncernen ca 120 st anställda.
+            <br><br>
+            Caland blev en koncern 2002 genom att Caland Betonghåltagning AB startades med specialisering inom betonghåltagning, sanering och rivning.
+            2007 utökades koncernen ytterligare med Caland Transport AB –Åkeri som utför en mängd olika transporttjänster.
+        </p>
+        <br><br><br><br>
+        <div class="timeline">
+            <div class="timeline-item">
+                <div class="timeline-icon">
+                    <i class="fa fa-check"></i>
+                </div>
+                <div class="timeline-content">
+                    <h4 class="timeline-content-date">1994</h4>
+                        <p>
+                            Caland är ett familjeägt företag som startades år 1994 genom det första bolaget Caland AB.
+                            Företagsnamnet är de tre första bokstäverna från familjenamnen Calais och Andersson,
+                            <span class="red">CAL</span> och <span class="red">AND</span> blev företagsnamnet CALAND.
+                        </p>
+                </div>
+            </div>
+            <div class="timeline-item">
+                <div class="timeline-icon">
+                    <i class="fa fa-check"></i>
+                </div>
+                <div class="timeline-content right">
+                    <h4 class="timeline-content-date">1994</h4>
+                        <p>
+                            Caland är ett familjeägt företag som startades år 1994 genom det första bolaget Caland AB.
+                            Företagsnamnet är de tre första bokstäverna från familjenamnen Calais och Andersson,
+                            <span class="red">CAL</span> och <span class="red">AND</span> blev företagsnamnet CALAND.
+                        </p>
+                </div>
+            </div>
+            <div class="timeline-item">
+                <div class="timeline-icon">
+                    <i class="fa fa-check"></i>
+                </div>
+                <div class="timeline-content">
+                    <h4 class="timeline-content-date">1994</h4>
+                        <p>
+                            Caland är ett familjeägt företag som startades år 1994 genom det första bolaget Caland AB.
+                            Företagsnamnet är de tre första bokstäverna från familjenamnen Calais och Andersson,
+                            <span class="red">CAL</span> och <span class="red">AND</span> blev företagsnamnet CALAND.
+                        </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
