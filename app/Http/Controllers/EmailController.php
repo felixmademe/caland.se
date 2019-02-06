e71fa5de487620bbff478835a596457d68e362d4
Felix Wetell <felix.wetell@gmail.com>
2019-02-06 11:43:35 +0100

    Started making the application mail #19

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -29,4 +29,13 @@ class EmailController extends Controller
         return redirect()->back()->with( 'success', 'Ditt email har skickats!' );
     }
 
+    public function applicationEmail( Request $request )
+    {
+        $request->validate( [
+            'name'    => 'required|string|max:255',
+            'email'   => 'required|email',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string'
+        ] );
+    }
 }
