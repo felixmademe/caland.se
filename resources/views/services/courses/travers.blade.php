125c933ea507cfbb377d7b5fcb0c3be9dc479d17
Felix Wetell <felix.wetell@gmail.com>
2019-02-08 15:54:13 +0100

    Added course views with content #24

---
+++ resources/views/services/courses/travers.blade.php
@@ -0,0 +1,35 @@
+@extends( 'layouts.app' )
+@section( 'title', 'Traversutbildning' )
+@section( 'content' )
+
+    <section class="course-about">
+        <h1>Traversutbildning</h1>
+        <p>
+            Vi på Caland är certifierade och erbjuder Traverutbildning som följer SS-ISO 9926-1.
+            <br><br>
+            <b>Kursinnehåll:</b>
+            <br>
+            Introduktion och terminologi, vad är vad? Konstruktion, hur vikt fördelas,
+            operatörens roll, användning. Kommunikation och lasthantering. Underhåll och kontroll.
+            Lagar, regler, ergonomi, säkerhet och olyckor.
+            <br><br>
+            <b>Vad säger lagen?</b>
+            <br>
+            Krav på denna utbildning ställs i Arbetsmiljöverkets föreskrift AFS:2006:6.
+            <br>
+            Utdrag från § 29 i denna föreskrift:
+            <br><br>
+            <span class="italic">”En arbetsgivare som låter en arbetstagare eller inhyrd arbetskraft använda
+            lyftanordningar eller lyftredskap skall ha dokumentation över dennes praktiska och
+            teoretiska kunskaper med avseende på säker användning av utrustningen.”</span>
+            <br><br>
+            Kontakta oss för mer information gällande Traverutbildning i Karlstad och Värmland.
+        </p>
+    </section>
+    <hr>
+    <section class="course-return">
+        <p>Klicka här för att gå tillbaka</p>
+        <a class="btn btn-red" href="{{ route( 'course' ) }}">Utbildningar</a>
+    </section>
+
+@endsection
---
+++ resources/views/services/courses/vehicle-crane.blade.php
@@ -0,0 +1,27 @@
+@extends( 'layouts.app' )
+@section( 'title', 'Fordonsmonterad kranutbildning' )
+@section( 'content' )
+
+    <section class="course-about">
+        <h1>Fordonsmonterad kranutbildning</h1>
+        <p>
+            <b>Fordonsmonterad Kranutbildning</b>
+            <br><br>
+            Utbildning där du lär dig att köra fordonsmonterad kran.
+            <br><br>
+            Fordonsmonterad kranar delas in i två kategorier: 1: Kranar med kapacitet upp till 18 tonmeter och timmerkranar oavsett storlek. 2: Kranar med kapacitet över 18 tonmeter.
+            <br><br>
+            Förkunskapskrav: Körkortsklass lägst *BC***. Utbildningen ger intyg för båda kategorierna.
+            <br><br>
+            Efter avslutad och godkänd utbildning erhålles intyg enligt TYA
+            <br><br>
+            Välkommen att kontakta oss när ni behöver utbildas i Fordonsmonterad Kranutbildning i Karlstad & Värmland.
+        </p>
+    </section>
+    <hr>
+    <section class="course-return">
+        <p>Klicka här för att gå tillbaka</p>
+        <a class="btn btn-red" href="{{ route( 'course' ) }}">Utbildningar</a>
+    </section>
+
+@endsection
