99d18d0f34c46c0040ea88cd10e007d0547a92c5
Felix Wetell <felix.wetell@gmail.com>
2019-02-25 17:06:21 +0100

    New design on service index #29

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -2,8 +2,17 @@
 @section( 'title', 'Tjänster' )
 @section( 'content' )
 
-    <div class="services">
+    <section class="service-header">
         <h1>Tjänster</h1>
+        <p>
+            Vi har resurser, metoder och kompetens att utföra
+            professionella tjänster inom byggbranschen.
+            <br>
+            <span class="underline">Bredden är vår styrka</span>.
+        </p>
+    </section>
+    <hr>
+    <section class="service-about">
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -11,32 +20,23 @@
             in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
             non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <ul class="list-group list-group-flush">
-            <a href="{{ route( 'concrete' ) }}" class="list-group-item">
-                Betonghåltagning
-            </a>
-            <a href="{{ route( 'transport' ) }}" class="list-group-item">
-                Transport
-            </a>
-            <a href="{{ route( 'staff' ) }}" class="list-group-item">
-                Bemanning
-            </a>
-            <a href="{{ route( 'course' ) }}" class="list-group-item">
-                Utbildning
-            </a>
-            <a href="{{ route( 'help' ) }}" class="list-group-item">
-                Bygghjälpen
-            </a>
-            <a href="{{ route( 'cleaning' ) }}" class="list-group-item">
-                Städ
-            </a>
-            <a href="{{ route( 'company-service' ) }}" class="list-group-item">
-                Företagsservice
-            </a>
-        </ul>
-        <div class="text-center my-3">
-            <img src="{{ asset( 'img/service/iso.jpg' ) }}" alt="ISO certifierade">
+        <div class="text-center">
+            <a class="btn btn-red btn-expand m-1" href="{{ route( 'concrete' ) }}">Betonghåltagning</a>
+            <a class="btn btn-red btn-expand m-1" href="{{ route( 'transport' ) }}">Transport</a>
+            <a class="btn btn-red btn-expand m-1" href="{{ route( 'staff' ) }}">Bemanning</a>
+            <a class="btn btn-red btn-expand m-1" href="{{ route( 'course' ) }}">Utbildning</a>
+            <a class="btn btn-red btn-expand m-1" href="{{ route( 'help' ) }}">Bygghjälpen</a>
+            <a class="btn btn-red btn-expand m-1" href="{{ route( 'cleaning' ) }}">Städ</a>
         </div>
-    </div>
+        <div class="text-center my-3">
+            <img src="{{ asset( 'img/services/iso.jpg' ) }}" alt="ISO certifierade">
+        </div>
+    </section>
+    <hr>
+    <section class="service-contact text-center">
+        <h3>Kontakt</h3>
+        <p>Är det något du undrar, tvecka inte att kontakta oss!</p>
+        <a class="btn btn-red btn-expand" href="{{ route( 'contact' ) }}">Kontakta oss</a>
+    </section>
 
 @endsection
