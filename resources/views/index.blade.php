98493d2a37113fbd1064b318e67a661aa74a6508
Felix Wetell <felix.wetell@gmail.com>
2019-02-25 20:17:28 +0100

    New design for landing page #28

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,55 +2,33 @@
 @section( 'title', 'Start' )
 @section( 'content' )
 
-    <header class="mobile-hero d-block d-md-none">
-        <img src="{{ asset( '/img/collection.jpg' ) }}" alt="">
-        <div class="text">
+    <header class="hero">
+        <img src="{{ asset( 'img/collection.jpg' ) }}" alt="">
+        <div class="text-box">
+            <div class="logo-wrapper">
+                <img class="img-fluid" src="{{ asset( '/img/logo.png' ) }}" alt="">
+            </div>
             <h1>Caland AB</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <a class="btn btn-red btn-expand" href="{{ route( 'about' ) }}">Läs mer</a>
-        </div>
-    </header>
-
-    <header class="hero d-none d-md-block">
-        <div class="d-flex flex-row">
-            <div class="col-md-6 text-half d-flex align-items-center">
-                <div class="">
-                    <h1>Caland AB</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                    <a class="btn btn-red" href="{{ route( 'about' ) }}">Läs mer</a>
-                </div>
-            </div>
-            <div class="col-md-6 img-half">
-                <img src="{{ asset( '/img/collection.jpg' ) }}" alt="">
-            </div>
+            <p>Bredden är vår styrka</p>
         </div>
     </header>
 
     <section class="company py-5">
-        <div class="col-md-8 offset-md-2">        
-            <h2>Tjänster</h2>
-            <p>Vi erbjuder ett flertal olika tjänster till både företag och privatpersoner.</p>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'concrete' ) }}">Betonghåltagning</a>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'transport' ) }}">Transport</a>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'staff' ) }}">Bemanning</a>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'course' ) }}">Utbildning</a>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'help' ) }}">Bygghjälpen</a>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'cleaning' ) }}">Städ</a>
-            <hr>
-            <h3>Jobb</h3>
-            <p>
-                Vi har flertal olika tjänster och söker alltid nya medarbetare. Kolla in tjänster vi har
-                och skicka in en intresseanmälan!
-            </p>
-            <a class="btn btn-red btn-expand m-1" href="{{ route( 'job' ) }}">Jobba på Caland</a>
+        <div class="col-md-8 offset-md-2">
+            <div class="row">
+                <div class="col-sm-6 p-2">
+                    <a class="btn btn-start" href="{{ route( 'services' ) }}">Våra tjänster</a>
+                </div>
+                <div class="col-sm-6 p-2">
+                    <a class="btn btn-start" href="{{ route( 'course' ) }}">Boka utbildning</a>
+                </div>
+                <div class="col-sm-6 p-2">
+                    <a class="btn btn-start" href="{{ route( 'contact' ) }}">Kontatka oss</a>
+                </div>
+                <div class="col-sm-6 p-2">
+                    <a class="btn btn-start" href="{{ route( 'job' ) }}">Jobba på Caland</a>
+                </div>
+            </div>
         </div>
     </section>
 
