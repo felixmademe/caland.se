d37cb21adcaa3c3443c707b00577928df4ffb341
Felix Wetell <felix.wetell@gmail.com>
2019-02-25 10:51:48 +0100

    Added error view layout

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,4 +1,4 @@
-@extends( 'layouts.app' )
+@extends( 'layouts.error' )
 @section( 'title', '401' )
 @section( 'content' )
 
--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -1,4 +1,4 @@
-@extends( 'layouts.app' )
+@extends( 'layouts.error' )
 @section( 'title', '429' )
 @section( 'content' )
 
