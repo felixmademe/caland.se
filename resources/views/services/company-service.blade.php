8dde000a6dea13ff86a1bf3dd7d4238d99291ade
Felix Wetell <felix.wetell@gmail.com>
2019-02-28 20:33:14 +0100

    Added content to "Företagsservice"

--- resources/views/services/company-service.blade.php
+++ resources/views/services/company-service.blade.php
@@ -2,4 +2,87 @@
 @section( 'title', 'Företagsservice' )
 @section( 'content' )
 
+    <section class="service-header">
+        <img src="{{ asset( '/img/caland-foretagsservice.jpg' ) }}" alt="Caland Företagsservice">
+        <br><br>
+        <h1>Företagsservice</h1>
+        <p>
+            Vi har resurser, metoder och kompetens att utföra
+            professionella tjänster inom byggbranschen.
+            <br>
+            <span class="underline">Bredden är vår styrka</span>.
+        </p>
+    </section>
+    <hr>
+    <section class="service-about">
+        <h2>Vad gör vi?</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <div class="row justify-content-center">
+            <ul class="nav nav-pills mb-2" id="pills-tab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active btn-red-border m-1" id="second-tab" data-toggle="pill" href="#second"
+                    role="tab" aria-controls="second" aria-selected="false">
+                        Bygghjälpen
+                    </a>
+                </li>
+            </ul>
+            <br>
+            <div class="tab-content px-2" id="myTabContent">
+                <div class="tab-pane active fade show" id="second" role="tabpanel" aria-labelledby="second-tab">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="text-center">
+                        <a class="btn btn-red btn-expand" href="{{ route( 'help' ) }}">Bygghjälpen</a>
+                    </div>
+                    <br>
+                    <img class="img-fluid" src="{{ asset( 'img/services/stad.jpg' ) }}" alt="Städservice">
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section class="service-contact">
+        <h3>Kontakta oss</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-fluid" src="{{ asset( '/img/staff/charlotta.jpg' ) }}" alt="Charlotta Löfgren">
+                <h4>Charlotta Löfgren</h4>
+                <p>
+                    <span class="red">Företagsservice</span>
+                    <br>
+                    Personalchef
+                    <br>
+                    054-22 07 94
+                    <br>
+                    <a class="link-effect" href="mailto:lotta@caland.se">lotta@caland.se</a>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img class="img-fluid" src="{{ asset( '/img/staff/ellinor.jpg' ) }}" alt="Ellinor Halldan">
+                <h4>Ellinor Halldan</h4>
+                <p>
+                    <span class="red">Företagsservice</span>
+                    <br>
+                    Konsultchef/HR
+                    <br>
+                    054-22 07 95
+                    <br>
+                    <a class="link-effect" href="mailto:ellinor@caland.se">ellinor@caland.se</a>
+                </p>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section class="service-return">
+        <p>Klicka här för att gå tillbaka</p>
+        <a class="btn btn-red btn-expand" href="{{ route( 'services' ) }}">Tjänster</a>
+    </section>
+
 @endsection
