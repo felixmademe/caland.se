bb30cbde63650746a7afc33119371aca7276db21
Felix Wetell <felix.wetell@gmail.com>
2019-02-28 21:17:25 +0100

    UI shows which view the user is on in navbar #38

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,37 +8,37 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( '/' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'index' ) }}">Start <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'tjänster/bemanning' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'staff' ) }}">Bemanning</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'tjänster/betonghåltagning' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'concrete' ) }}">Betonghåltagning</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'tjänster/bygghjälpen' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'help' ) }}">Bygghjälpen</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'tjänster/städ' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'cleaning' ) }}">Städ</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'tjänster/transport' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'transport' ) }}">Transport</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'tjänster/utbildning' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'course' ) }}">Utbildning</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'jobb' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'job' ) }}">Jobb</a>
             </li>
         </ul>
         <div class="dropdown-divider d-block d-lg-none"></div>
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'kontakt' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'contact' ) }}">Kontakt</a>
             </li>
-            <li class="nav-item link-effect">
+            <li class="nav-item link-effect {{ Request::is( 'om-oss' ) ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route( 'about' ) }}">Om oss</a>
             </li>
         </ul>
--- resources/views/services/courses/index.blade.php
+++ resources/views/services/courses/index.blade.php
@@ -56,7 +56,7 @@
             </a>
         </div>
         <div class="text-center my-3">
-            <img src="{{ asset( 'img/service/iso.jpg' ) }}" alt="">
+            <img src="{{ asset( 'img/services/iso.jpg' ) }}" alt="">
         </div>
     </section>
     <hr>
