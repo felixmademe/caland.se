ae5d9eb31240598d9d34948e0c631338c7e30e86
Felix Wetell <felix.wetell@gmail.com>
2019-03-01 10:35:43 +0100

    Added link to facebook page #39

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -43,6 +43,8 @@
                     054-86 06 80
                     <br>
                     {{ env( 'MAIL_FROM_ADRESS', 'info@caland.se' ) }}
+                    <br>
+                    <a href="https://www.facebook.com/calandkoncernen" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
                 </p>
             </div>
         </div>
