a26f347e80788dbe1a6f25e22dd5bf8515c809d1
Felix Wetell <felix.wetell@gmail.com>
2019-03-05 18:57:56 +0100

    New Google Analytics ID #40

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,13 +49,13 @@
 
         <title>@yield( 'title' ) - {{ config('app.name', 'Caland AB') }}</title>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135535698-4"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135643404-1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'UA-135535698-4');
+          gtag('config', 'UA-135643404-1');
         </script>
     </head>
     <body>
