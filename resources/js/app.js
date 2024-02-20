dca14100158ea1cf676cf3c2808a59ce81d4dfd4
Felix Wetell <felix.wetell@gmail.com>
2024-02-20 23:19:28 +0100

    Adds ability to open tab on link click

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,20 +11,6 @@ $( '#list-tab a' ).on('click', function ( e )
     $( this ).tab( 'show' )
 } );
 
-if( window.location.hash )
-{
-    var hash = window.location.hash.substring(1);
-    if( hash == 'asbest' )
-    {
-        var container = $( '.underline' )[ 0 ];
-        $( '#asbestsanering-tab' ).click();
-        container.scrollIntoView( {
-                inline: 'end',
-                behavior: 'smooth',
-        } );
-    }
-}
-
 $( document ).on('click', '[data-toggle="lightbox"]', function(event)
 {
     event.preventDefault();
@@ -35,18 +21,37 @@ $( document ).on('click', '[data-toggle="lightbox"]', function(event)
 
 } );
 
-if (/#asbestsanering/.test(window.location.href)) {
-    $('#asbestsanering-tab').click();
-}
-if (/#rivning/.test(window.location.href)) {
-    $('#rivning-tab').click();
-}
-if (/#slipning/.test(window.location.href)) {
-    $('#slipning-tab').click();
-}
-if (/#maskinpark/.test(window.location.href)) {
-    $('#maskinpark-tab').click();
-}
-if (/#transport/.test(window.location.href)) {
-    $('#transport-tab').click();
-}
+$(document).ready(function(e)
+{        
+    if (window.location.toString().includes('#'))
+    {
+        var windowTab = GetTabFromWindow()
+        TabToOpen(windowTab, e);
+    }
+
+    $(".dropdown-item").click(function(event)
+    {
+        var tab = GetTabFromAnchor($(this));
+        TabToOpen(tab, event);
+    });
+    
+    function GetTabFromWindow()
+    {
+        var href = window.location.href.toString();
+        var array = href.split('#');
+        return array[array.length - 1];
+    }
+
+    function GetTabFromAnchor($element)
+    {
+        var href = $element.attr('href');
+        var array = href.split('#');
+        return array[array.length - 1];
+    }
+
+    function TabToOpen(tabToClick)
+    {
+        $('#' + tabToClick + '-tab').click();
+    }
+});
+
