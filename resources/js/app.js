9906f965489be3429d7fee1b17b7eb11f496df96
Felix Wetell <felix.wetell@gmail.com>
2024-02-20 20:09:45 +0100

    Rmeoves unsued js/css files and moves js/css files to correct folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,52 @@
-import './bootstrap';
+'use strict';
+
+console.log("Designed and developed by felix made me.")
+console.log("https://felixmade.me")
+
+const observer = lozad();
+observer.observe();
+
+$( '#list-tab a' ).on('click', function ( e )
+{
+    $( this ).tab( 'show' )
+} );
+
+if( window.location.hash )
+{
+    var hash = window.location.hash.substring(1);
+    if( hash == 'asbest' )
+    {
+        var container = $( '.underline' )[ 0 ];
+        $( '#asbestsanering-tab' ).click();
+        container.scrollIntoView( {
+                inline: 'end',
+                behavior: 'smooth',
+        } );
+    }
+}
+
+$( document ).on('click', '[data-toggle="lightbox"]', function(event)
+{
+    event.preventDefault();
+    $(this).ekkoLightbox(
+    {
+        maxWidth: 9999
+    } );
+
+} );
+
+if (/#asbestsanering/.test(window.location.href)) {
+    $('#asbestsanering-tab').click();
+}
+if (/#rivning/.test(window.location.href)) {
+    $('#rivning-tab').click();
+}
+if (/#slipning/.test(window.location.href)) {
+    $('#slipning-tab').click();
+}
+if (/#maskinpark/.test(window.location.href)) {
+    $('#maskinpark-tab').click();
+}
+if (/#transport/.test(window.location.href)) {
+    $('#transport-tab').click();
+}
