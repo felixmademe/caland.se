69ef29f83f2bcd33587d5b4a8a613966eab5a421
Felix Wetell <felix.wetell@gmail.com>
2025-03-19 19:36:22 +0100

    Adds models

---
+++ app/Models/Mail/Contact.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Contact extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+    public $email;
+    public $subject;
+    public $text;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct( $name, $email, $subject, $text )
+    {
+        $this->name    = $name;
+        $this->email   = $email;
+        $this->subject = $subject;
+        $this->text    = $text;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view( 'mail.contact' )
+                    ->from( 'no-reply@caland.se', config('app.name') )
+                    ->replyTo( $this->email )
+                    ->subject( $this->subject );
+    }
+}
